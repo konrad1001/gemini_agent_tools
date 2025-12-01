43e2c06802d5b8af3ce9d9f850d3bc9b5e99b4c0
Konrad Painta <konradpainta@gmail.com>
2025-12-01 18:20:12 +0000

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.3
+
+- Modified pubspec.yaml
+
 ## 0.0.2
 
 - Modified pubspec.yaml
--- lib/builder.dart
+++ lib/builder.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:async';
 
 import 'package:analyzer/dart/element/type.dart';
 import 'package:gemini_agent_tools/tool_annotation.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:build/build.dart';
 
 Builder toolGenerator(BuilderOptions options) =>
@@ -12,19 +14,19 @@ Builder toolGenerator(BuilderOptions options) =>
 class ToolGenerator extends GeneratorForAnnotation<Tool> {
   @override
   FutureOr<String> generateForAnnotatedElement(
-    Element element,
+    Element2 element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    final classElement = element as ClassElement;
+    final classElement = element as ClassElement2;
 
     final name = annotation.read('name').stringValue;
     final description = annotation.read('description').stringValue;
-    final fields = classElement.fields.where((f) => !f.isStatic);
+    final fields = classElement.fields2.where((f) => !f.isStatic);
 
     final properties = fields
         .map((f) {
-          final fieldName = f.name;
+          final fieldName = f.name3;
           if (fieldName == null) {
             throw Exception("Error: Found a field with a null name");
           }
@@ -42,7 +44,7 @@ class ToolGenerator extends GeneratorForAnnotation<Tool> {
             .join(',') ??
         fields
             .map((f) {
-              final fieldName = f.name;
+              final fieldName = f.name3;
               if (fieldName == null) {
                 throw Exception("Error: Found a field with a null name");
               }
